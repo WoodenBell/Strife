44b9dd4c3dfac20921dc7a258b6be4f108c1f23d
WoodenBell <codingcookie@gmail.com>
2017-07-10 13:21:58 -0300

    Creation

---
+++ src/io/github/woodenbell/strife/model/EditorModel.java
@@ -0,0 +1,51 @@
+package io.github.woodenbell.strife.model;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class EditorModel {
+	public boolean save(EditItem f) {
+			try {
+				Files.write(f.getEditFile().getPath(), f.getContent());
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+	}
+
+
+	public boolean saveAs(EditItem f, Path choosenPath) {
+		if(choosenPath.toFile().exists() && choosenPath.toFile().isFile()) {
+			try {
+				Files.write(choosenPath, f.getContent(), StandardOpenOption.CREATE_NEW);
+				f.setFileSaved(true);
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		} else {
+			try {
+				Files.write(choosenPath, f.getContent(), StandardOpenOption.CREATE);
+				return true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+	}
+
+	public IOResult<EditFile> load(Path file) {
+		try {
+			List<String> lines = Files.readAllLines(file);
+			return new IOResult<EditFile>(true, new EditFile(file.getFileName().toString(), file, lines));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new IOResult<EditFile>(false, null);
+		}
+	}
+}
